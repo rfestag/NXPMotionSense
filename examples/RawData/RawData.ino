63fc1157c3779284408c3090afa88b40149b3550
PaulStoffregen <paul@pjrc.com>
2016-03-09 02:38:26 -0800

    Minor changes in RawData example

--- examples/RawData/RawData.ino
+++ examples/RawData/RawData.ino
@@ -1,39 +1,42 @@
 #include <NXPMotionSense.h>
+#include <Wire.h>
+#include <EEPROM.h>
 
 NXPMotionSense imu;
 
 void setup()
 {
+  Serial.begin(115200);
   while (!Serial) ; // wait for serial port open
   imu.begin();
 }
 
 void loop()
 {
-  int16_t data[3];
+  int16_t accel[3], gyro[3], mag[3];
 
   if (imu.available()) {
     // get and print data
-    imu.readRawAccelerometer(data);
-    Serial.print(data[0]);
+    imu.readRawAccelerometer(accel);
+    imu.readRawGyroscope(gyro);
+    imu.readRawMagnetometer(mag);
+    Serial.print(accel[0]);
     Serial.print(',');
-    Serial.print(data[1]);
+    Serial.print(accel[1]);
     Serial.print(',');
-    Serial.print(data[2]);
+    Serial.print(accel[2]);
     Serial.print(',');
-    imu.readRawGyroscope(data);
-    Serial.print(data[0]);
+    Serial.print(gyro[0]);
     Serial.print(',');
-    Serial.print(data[1]);
+    Serial.print(gyro[1]);
     Serial.print(',');
-    Serial.print(data[2]);
+    Serial.print(gyro[2]);
     Serial.print(',');
-    imu.readRawMagnetometer(data);
-    Serial.print(data[0]);
+    Serial.print(mag[0]);
     Serial.print(',');
-    Serial.print(data[1]);
+    Serial.print(mag[1]);
     Serial.print(',');
-    Serial.print(data[2]);
+    Serial.print(mag[2]);
     Serial.println();
   }
 
