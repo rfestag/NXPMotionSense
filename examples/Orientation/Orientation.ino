a6f0d506b6e84ed8849439491c86a5e6dd4e6be7
PaulStoffregen <paul@pjrc.com>
2016-03-16 03:42:24 -0700

    Add NXP Sensor Fusion algorithm and Orientation example

---
+++ examples/Orientation/Orientation.ino
@@ -0,0 +1,74 @@
+// Full orientation sensing using NXP's advanced sensor fusion algorithm.
+//
+// You *must* perform a magnetic calibration before this code will work.
+//
+// To view this data, use the Arduino Serial Monitor to watch the
+// scrolling angles (in radians: 1.57 is 90 degrees).
+//
+// For graphical display, this Processing sketch works:
+// https://www.arduino.cc/en/Tutorial/Genuino101CurieIMUOrientationVisualiser
+
+#include <NXPMotionSense.h>
+#include <Wire.h>
+#include <EEPROM.h>
+
+NXPMotionSense imu;
+NXPSensorFusion filter;
+
+void setup() {
+  Serial.begin(9600);
+  imu.begin();
+  filter.begin();
+}
+
+void loop() {
+  float ax, ay, az;
+  float gx, gy, gz;
+  float mx, my, mz;
+  float roll, pitch, heading;
+
+  if (imu.available()) {
+    // Read the motion sensors
+    imu.readMotionSensor(ax, ay, az, gx, gy, gz, mx, my, mz);
+
+    // Update the SensorFusion filter
+    filter.update(gx, gy, gz, ax, ay, az, mx, my, mz);
+  }
+
+  if (readyToPrint()) {
+    // print the heading, pitch and roll
+    roll = filter.getRoll();
+    pitch = filter.getPitch();
+    heading = filter.getYaw();
+    Serial.print(heading);
+    Serial.print(",");
+    Serial.print(pitch);
+    Serial.print(",");
+    Serial.println(roll);
+  }
+}
+
+
+// Decide when to print
+bool readyToPrint() {
+  static unsigned long nowMillis;
+  static unsigned long thenMillis;
+
+  // If the Processing visualization sketch is sending "s"
+  // then send new data each time it wants to redraw
+  while (Serial.available()) {
+    int val = Serial.read();
+    if (val == 's') {
+      thenMillis = millis();
+      return true;
+    }
+  }
+  // Otherwise, print 8 times per second, for viewing as
+  // scrolling numbers in the Arduino Serial Monitor
+  nowMillis = millis();
+  if (nowMillis - thenMillis > 125) {
+    thenMillis = nowMillis;
+    return true;
+  }
+  return false;
+}
